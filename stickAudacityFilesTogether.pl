0d03580b418886aff505d199b6811a1346d813b7
philip r brenan <philiprbrenan@users.noreply.github.com>
2017-01-22 23:49:37 +0000

    Add files via upload

--- stickAudacityFilesTogether.pl
+++ stickAudacityFilesTogether.pl
@@ -66,15 +66,30 @@ From the command line, execute:
 =cut
 
 # User configuration start
-my $project  = 'testProject';                                                   # Audacity project name so we can find files to stick together
-my $home     = '/home/phil/z/Fiverr/'.$project.'/';                             # Folder containing Audacity project
-my $in       = $home.$project.'_data';                                          # Audacity folder containing sound files - default computed from above two lines should be ok
-my $out      = $home.'out/';                                                    # Output directory
-my $rate     = 22050;                                                           # Recording rate
-   $rate     = 44100;                                                           # Recording rate
-my $channels = 1;                                                               # Number of channels
-my $numberOfFilesToConcatenate = 1000;                                          # Up to this many files will be concatenated at a time to form an output file
+my $project  = 'testProject';                                                   # Audacity project name so we can find files to stick together - CHECK THIS VALUE
+my $audacity = '/home/phil/z/z/Fiverr/audacity/';                               # Folder containing Audacity project - CHECK THIS VALUE
+
+my $rate     = 22050;                                                           # Recording rate - CHECK THIS VALUE
+   $rate     = 44100;
+my $channels = 2;                                                               # Number of channels = - CHECK THIS VALUE - it should be 1 for mono or 2 for stereo which means that pairs of files will be merged before being concatenated - if you get it wrong your recording will come out at the wrong speed
+
+if ($^O !~ /linux/i)                                                            # Windows user defaults
+ {$project  = 'ab2016';
+  $audacity = 'C:/Users/Sawan/Desktop/';
+  $channels = 2;
+ }
+
+# The following fields are probably ok
+my $numberOfFilesToConcatenate = 100;                                           # Up to this many files will be concatenated at a time to form an output file
 my $restartAtBlock             = 1;                                             # In the event that something goes wrong, we can restart at a specific output file, counting from 1 as the first block
+
+# The following fields should be ok as set
+my $home     = $audacity.$project.'/';                                          # Folder containing audacity project
+my $in       = $home.$project.'_data';                                          # Audacity folder containing sound files
+my $out      = $home.'out/';                                                    # Output directory
+my $tmp      = $home.'tmp/';                                                    # Temporary file directory
+
+my $version  =  '2017.01.20';
 # User configuration end
 
 use warnings FATAL => qw(all);
@@ -82,6 +97,16 @@ use strict;
 use File::Glob qw(:bsd_glob);
 use Data::Dump qw(dump);
 use File::Path qw(make_path);
+
+say STDERR "Stick Audacity files together $version\n",
+"Scanning   \"$in\" for .au files\n",
+"Writing to \"$out\"\n",
+"Rate=$rate hz, channels=$channels";
+
+if (1)                                                                          # Check for sox
+ {my $s = qx(sox --version 2>&1);
+  $s =~ /SoX/ or die "Please install Sox as it does not seem to be present";
+ }
                                                                                 # .au files
 my @files = map  {$_->[0]}
             sort {$a->[1] <=> $b->[1]}
@@ -102,7 +127,7 @@ for(bsd_glob("$out/*.wav"))                                                     
 
 if (my $nFiles = scalar(@files))                                                # Number of files
  {my $n = $numberOfFilesToConcatenate;                                          # Up to this many files will be concatenated at a time to form an output file
-  make_path($out);
+  make_path($_) for $out, $tmp;
   say STDERR "Processing $nFiles .au files from: $in in blocks of $n";
 
   my @f = ([]);                                                                 # Blocks of files to convert
@@ -114,16 +139,45 @@ if (my $nFiles = scalar(@files))                                                
   my $N = @f;
   for(1..@f)                                                                    # Convert and concatenate each block of input files
    {next if $_ < $restartAtBlock;                                               # Restart if necessary
-    my $f = join ' ', @{$f[$_-1]};                                              # Input file block
+    my @f = @{$f[$_-1]};                                                        # Input file block
+
+    if ($channels == 2)                                                         # Merge channels if necessary
+     {if (@f % 2)
+       {warn "Odd number of files in block and stereo merge requested, last file has been duplicated";
+        push @f, $f[-2];
+       }
+      my @F;
+      while(@f)                                                                 # Each pair of files
+       {my $f1 = shift @f;
+        my $f2 = shift @f;
+        my $t = $tmp.sprintf("%08d.wav", scalar(@F)+1);                         # Temporary output file name
+        unlink $t;
+        my $c = "sox --combine merge -b 32  -c 1 -e floating-point -r $rate -L  $f1 $f2 $t";
+        say STDERR $c;                                                          # Command
+        say STDERR qx($c);                                                      # Execute
+        if ($!)
+         {say STDERR "Error during sox merge: $!";
+         }
+        else {push @F, $t}                                                      # Save merged file
+       }
+      @f = @F;                                                                  # Merged files to be concatenated
+     }
+
+    my $f = join ' ', @f;                                                       # Input file block joined together
     my $o = $out.sprintf("%08d.wav", $_);                                       # Output file name
     unlink $o;
     say STDERR " $_/$N";
-    say STDERR qx(sox -b 32  -c $channels -e floating-point -r $rate -L  $f $o);# Convert and concatenate
+    my $c = "sox -b 32  -c $channels -e floating-point -r $rate -L  $f $o";     # Convert and concatenate
+    say STDERR $c;                                                              # Command
+    say STDERR qx($c);                                                          # Execute
+    if ($!)
+     {say STDERR "Error during sox concatenate: $!";
+     }
    }
-  say STDERR "$N output files in:\n$out";
+  say STDERR "SUCCESS: $N output files in:\n  $out";
   exit 0                                                                        # Success
  }
 else                                                                            # No input files
- {say STDERR "Unable to find any .au files under folder:\n$in";
+ {say STDERR "FAIL: Unable to find any .au files under folder:\n$in";
   exit 1                                                                        # Error
  }
